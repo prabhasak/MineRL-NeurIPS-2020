7799c8eb059063e96bd1dc71e59bccf8fa325b31
prabhasak <prabhasa.94@gmail.com>
2020-09-13 00:15:45 -0500

    fixed cnn implementation

--- envs/wrappers/__init__.py
+++ envs/wrappers/__init__.py
@@ -5,13 +5,17 @@ from envs.wrappers.discrete_action_wrapper import MineRLDiscreteActionWrapper
 from envs.wrappers.gray_scale_wrapper import MineRLGrayScale
 from envs.wrappers.observation_stack_wrapper import MineRLObservationStack
 from envs.wrappers.observation_wrapper import MineRLObservationWrapper
+from envs.wrappers.observation_wrapper import MineRLPOVWithVectorWrapper
 from envs.wrappers.time_limit_wrapper import MineRLTimeLimitWrapper
 
 
-def wrap(env, discrete=False, num_actions=32, data_dir=None, num_stack=1, action_repeat=1,
+def wrap(env, conv=False, discrete=False, num_actions=32, data_dir=None, num_stack=1, action_repeat=1,
          gray_scale=False, seed=None):
     env = MineRLTimeLimitWrapper(env)
-    env = MineRLObservationWrapper(env)
+    if conv:
+        env = MineRLPOVWithVectorWrapper(env) # With Conv layers
+    else:
+        env = MineRLObservationWrapper(env) # Without Conv layers
     env = MineRLActionWrapper(env)
     if discrete:
         env = MineRLDiscreteActionWrapper(env, num_actions, data_dir=data_dir)
