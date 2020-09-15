add4b2f273f3ab71654dddc5d9f3a9c9e4fcdc5a
prabhasak <prabhasa.94@gmail.com>
2020-09-15 02:24:38 -0500

    generated expert data v2

--- rl_algorithms/dqn/agent.py
+++ rl_algorithms/dqn/agent.py
@@ -245,7 +245,7 @@ class DQNAgent(Agent):
 
         for self.i_episode in range(1, self.args.episode_num + 1):
             state = self.env.reset()
-            Added by PK: flatten state
+            # Added by PK: flatten state
             if 'MineRL' in self.env_info['name']:
                 state = self.MineRL_flatten_states(state)
             self.episode_step = 0
